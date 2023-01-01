726704d1affdba9b0dba4967b2df27ef6c544f64
EC2 Default User <ec2-user@ip-172-31-86-191.ec2.internal>
2023-01-01 05:37:29 +0000

    first

---
+++ provider.tf
@@ -0,0 +1,8 @@
+#Update provider
+#AWS as our provider
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIARWNEGFWXTH3CSHED"
+  secret_key = "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"
+}
